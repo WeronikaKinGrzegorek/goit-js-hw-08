5010513064c4fab46273588877988d0125008d3d
WeronikaKinGrzegorek <126391598+WeronikaKinGrzegorek@users.noreply.github.com>
2023-08-19 13:12:27 +0200

    completed hw 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+let listArray = [];
+
+galleryItems.forEach(galleryItem => {
+  const listItem = document.createElement('li');
+  const innerString = `<a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+  </a>`;
+  listItem.insertAdjacentHTML('beforeend', innerString);
+  listArray.push(listItem);
+});
+gallery.append(...listArray);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+);
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(storedTime);
